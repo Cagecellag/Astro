d6184f74c6fe9d62d9c7c218e7c04445c3b7e647
CageCell <nokesko@outlook.com>
2025-08-18 22:37:00 +0200

    Addted a damn sidebar

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+document.getElementById("open-btn").addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+});
+
+document.getElementById("close-btn").addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+});
